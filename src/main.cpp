9e456c760f45337a75193b784db37c68cb20b1f4
3rickDJ <erickdejesusmolina@gmail.com>
2023-05-16 07:14:06 -0600

    prototype some endpoints get asientos. patch asiento

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 #include <nlohmann/json.hpp>
 #include <iomanip>
 #include "router.h"
+#include <string>
+#include <fstream>
 
 using json = nlohmann::json;
 using namespace route;
+
+json data;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -57,7 +61,40 @@ int main() {
     if (match.test("/books/:id/hola/:coma")) {
         std::cout << "💌 first: " << *match.get("id") << " second: " << *match.get("coma");
     };
-    
+    std::cout << "\n🌟🌟🌟🌟\n\n";
+    std::ifstream f("cine.json");
+    data = json::parse(f);
+    match = router.set("/sala/1/horario/3/asientos");
+
+    // endpoint de asientos de una sala en un determinado horario
+    if(match.test("/sala/:sala/horario/:horario/asientos")){
+        auto sala_s = match.get("sala");
+        int sala = std::stoi(sala_s->c_str());
+        auto horario_s = match.get("horario");
+        int horario = std::stoi(horario_s->c_str());
+        std::cout << "sala: " << sala << " horario: " << horario << '\n';
+        auto asientos = data["salas"][sala]["horarios"][horario]["asientos"];
+        std::cout << asientos;
+//        printf("\n\n%s", asientos.dump().c_str());
+    }
+    data["salas"][1]["horarios"][1]["asientos"][9]["estado"] = "🚩";
+    std::cout << data.dump(2).c_str();
+
+    match = router.set("/sala/1/horario/3/asientos/9/");
+    if(match.test("/sala/:sala/horario/:horario/asientos/:asiento/")){
+        std::cout << "\n🐳             🐳\n";
+        auto sala_s = match.get("sala");
+        int sala = std::stoi(sala_s->c_str());
+        auto horario_s = match.get("horario");
+        int horario = std::stoi(horario_s->c_str());
+        auto asiento_s = match.get("asiento");
+        std::cout << "🐥 asiento_s " << asiento_s->c_str() << "\n\n";
+        int asiento = std::stoi(asiento_s->c_str());
+        std::cout << "sala: " << sala << " horario: " << horario << " asiento: " << asiento << '\n';
+        data["salas"][sala]["horarios"][horario]["asientos"][asiento]["estado"] = "💋query params";
+//        std::cout << new_data.dump().c_str();
+        std::cout << data.dump().c_str();
+    }
 
     return 0;
 }
